ffbae7daa2cefcf4f40e8b1a150d7e6608a81693
Yermochenko Sergey <yermochenko@gmail.com>
2016-05-03 11:21:31 +0300

    Для вариантов использования "Add new employee", "Change user name or role of employee" и "Dissmiss employee" при работе с базой данных для действий, подразумевающих выполнение нескольких последовательных SQL-запросов, реализовано выполнение этих действий в рамках одной транзакции

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -31,28 +31,54 @@ public class UserService {
 	}
 
 	public void save(User user) throws SQLException {
-		if(user.getRole() != Role.CLIENT) {
-			if(user.getId() == null) {
-				user.setPassword("12345");
-				Integer id = db.create(user);
-				user.setId(id);
-			} else {
-				User oldUser = db.read(user.getId());
-				if(oldUser != null && oldUser.getRole() != Role.CLIENT) {
-					user.setPassword(oldUser.getPassword());
-					db.update(user);
+		try {
+			connection.setAutoCommit(false);
+			if(user.getRole() != Role.CLIENT) {
+				if(user.getId() == null) {
+					user.setPassword("12345");
+					Integer id = db.create(user);
+					user.setId(id);
+				} else {
+					User oldUser = db.read(user.getId());
+					if(oldUser != null && oldUser.getRole() != Role.CLIENT) {
+						user.setPassword(oldUser.getPassword());
+						db.update(user);
+					}
 				}
 			}
+			connection.commit();
+		} catch(SQLException e) {
+			try {
+				connection.rollback();
+			} catch(SQLException e1) {}
+			throw e;
+		} finally {
+			try {
+				connection.setAutoCommit(true);
+			} catch(SQLException e) {}
 		}
 	}
 
 	public void delete(List<Integer> ids) throws SQLException {
-		User user;
-		for(Integer id : ids) {
-			user = db.read(id);
-			if(user.getRole() != Role.CLIENT) {
-				db.delete(id);
+		try {
+			connection.setAutoCommit(false);
+			User user;
+			for(Integer id : ids) {
+				user = db.read(id);
+				if(user.getRole() != Role.CLIENT) {
+					db.delete(id);
+				}
 			}
+			connection.commit();
+		} catch(SQLException e) {
+			try {
+				connection.rollback();
+			} catch(SQLException e1) {}
+			throw e;
+		} finally {
+			try {
+				connection.setAutoCommit(true);
+			} catch(SQLException e) {}
 		}
 	}
 
